e51e5153d4c46106e3cc74ecb5b26d9b07783b4c
shivam singh <shivamsingh842314@gmail.com>
2026-01-25 10:02:09 +0000

    how can i get these all files on my laptop

--- src/components/ExtraClassDialog.tsx
+++ src/components/ExtraClassDialog.tsx
@@ -136,7 +136,7 @@ export function ExtraClassDialog({ isOpen, onClose, extraClass }: ExtraClassDial
                         </Button>
                       </FormControl>
                     </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
+                    <PopoverContent className="p-0" align="start">
                       <Calendar
                         mode="single"
                         selected={field.value}
